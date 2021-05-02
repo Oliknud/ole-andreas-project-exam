31db8401da0bafd65617247307e2ec84553dfa40
Oliknud <69842780+Oliknud@users.noreply.github.com>
2021-05-02 21:57:48 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-const url = "https://knudsenweb.no/wp-json/wp/v2/posts?_embed";
-const main = document.querySelector(".main-home");
+const url = "https://knudsenweb.no/wp-json/wp/v2/posts?_embed=true";
+const postContainer = document.querySelector(".posts");
+const singlePost = document.querySelector(".single-post");
 
 
 function blogList(posts) {
     for (post of posts) {
         let img = post._embedded["wp:featuredmedia"][0].source_url;
-        console.log(post.title.rendered);
-        main.innerHTML += `<img src="${img}">`
+        console.log(img);
+        postContainer.innerHTML += `<div class="single-post">
+                                        <img src="${img}">
+                                        <div>
+                                            <h2>${}</h2>
+                                            <p></p>
+                                        </div>
+                                    </div>`
     }
 }
 
