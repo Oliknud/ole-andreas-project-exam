8ab5161f3dace3cbb050c53dbd6af9bfca14f7a4
Oliknud <69842780+Oliknud@users.noreply.github.com>
2021-05-09 03:33:25 +0200

    .

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,16 @@
 const menuLinks = document.querySelector(".menu-links");
-const links = document.querySelector(".links");
 const x = document.querySelector("#burger");
-const footer = document.querySelector("footer");
-const main = document.querySelector("main");
 const cross = document.querySelector(".cross");
+const body = document.querySelector("body");
 
 x.addEventListener("click", burgermenu);
 cross.addEventListener("click", burgermenu);
 
-
-
-
 function burgermenu() {
-    if (menuLinks.style.display === "none") {
-        menuLinks.style.display = "flex";
-        menuLinks.style.top = "0";
-        footer.style.display = "none";
-        main.style.display = "none";
+    menuLinks.classList.toggle("menu-links-active");
+    if (body.style.overflow === "hidden") {
+        body.style.overflow = "visible";
     } else {
-        menuLinks.style.display = "none";
-        menuLinks.style.top = "-100%";
-        footer.style.display = "flex"
-        main.style.display = "flex";
+        body.style.overflow = "hidden";
     }
 }
-
-console.log(x)
