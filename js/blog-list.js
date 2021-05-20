5a7b0498fe6f91855ede72d471ab758839097546
Oliknud <69842780+Oliknud@users.noreply.github.com>
2021-05-20 23:30:32 +0200

    .

--- js/blog-list.js
+++ js/blog-list.js
@@ -1,21 +1,25 @@
 const url = "https://knudsenweb.no/wp-json/wp/v2/posts?_embed=true";
 const postContainer = document.querySelector(".posts");
 const singlePost = document.querySelector(".single-post");
+const loadMore = document.querySelector(".load-more");
 
 // Fetch and display list of posts
 function blogList(posts) {
     for (post of posts) {
         let img = post._embedded["wp:featuredmedia"][0].source_url;
-        console.log(img);
+        
         postContainer.innerHTML += `<div class="single-post">
                                         <img src="${img}">
                                         <div class="single-post-text">
                                             <h2>${post.title.rendered}</h2>
-                                            <a href="blogspecific.html?id=${post.id}" class="read-more">Les mer</a>
+                                            <a href="blogspecific.html?id=${post.id}" class="read-more">View</a>
                                         </div>
                                     </div>`
     }
 }
+
+
+
 
 fetch(url)
     .then(response => response.json())
