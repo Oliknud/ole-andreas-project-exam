ad71e692b0eb9d41832f9a05ec2128121ecdbcaa
Oliknud <69842780+Oliknud@users.noreply.github.com>
2021-05-27 20:27:02 +0200

    ....

--- js/blog-specific.js
+++ js/blog-specific.js
@@ -5,15 +5,31 @@ const id = param.get("id");
 const url = `https://knudsenweb.no/wp-json/wp/v2/posts/${id}?_embed=true`;
 const specificH1 = document.querySelector(".h1-specific");
 const specifictPost = document.querySelector(".specific-post");
+const modal = document.querySelector(".modal-img");
 
 // Fetching specific blog post
 function blogSpecific(post) {
     specificH1.innerHTML = post.title.rendered;
     let img = post._embedded["wp:featuredmedia"][0].source_url;
-    specifictPost.innerHTML += `<img src="${img}">
-                                <p>${post.content.rendered}</p>`
-}
+    specifictPost.innerHTML += `<p>${post.content.rendered}</p>`
+    modal.innerHTML += `<img src="${img}">`
 
+    modal.addEventListener("click", function(){
+        modal.classList.toggle("modal-active");
+        if (specifictPost.style.display === "none") {
+            specifictPost.style.display = "block";
+        } else {
+            specifictPost.style.display = "none";
+        }
+    });
+
+    // body.addEventListener("click", function(){
+    //     if (modal.classList.contains("modal-active")) {
+    //         modal.classList.remove("modal-active");
+    //     }
+    // }) 
+    
+}
 
 
 fetch(url)
